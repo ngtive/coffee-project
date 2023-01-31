6666748d4b99107f25f66fc8485c5b0b1dfec4d2
Amir Hemmati <ngtive@protonmail.ch>
2023-02-01 00:09:46 +0330

    Update

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -11,7 +11,7 @@ class BrandController extends Controller {
 
     public function __construct() {
 
-        $this->middleware('auth:admin-api')->except(['index', 'show', 'products']);
+        $this->middleware('auth:admin-api')->except(['index', 'show', 'products', 'activeIndex']);
     }
 
     /**
